e70855e526dc6eb8534d6e0018c9db71b8d9b73e
jimmy <jimmy@jimmydeMacBook-Pro.local>
2016-06-25 16:52:06 +0800

    add global gem

--- other/webpack_sample/App.js
+++ other/webpack_sample/App.js
@@ -8,4 +8,5 @@ class App extends React.Component{
   }
 }
 
+
 export default App
--- other/webpack_sample/main.js
+++ other/webpack_sample/main.js
@@ -2,4 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+class App1 extends React.Component{
+
+  constructor(){
+    super();
+    this.state = { count: 0 }
+    this.update = this.update.bind(this);
+  }
+
+  update(){
+    this.setState({count: this.state.count + 1});
+  }
+
+  componentWillMount(){
+    console.warn("mounting");
+  }
+
+  componentDidMount(){
+    console.warn("mounted");
+  }
+
+  componentWillUnmount(){
+    console.warn("bye");
+  }
+  render(){
+    console.warn("rendering");
+    return <button onClick={this.update}>{this.state.count}</button>
+  }
+
+}
+
+class Wrapper extends React.Component{
+
+  constructor(){
+    super();
+  }
+
+  mount(){
+    ReactDOM.render(<App1 />, document.getElementById('app'));
+  }
+
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('app'));
+  }
+
+  render(){
+    return <div>
+      <button onClick={this.mount.bind(this)}> Mount</button>
+      <button onClick={this.unmount.bind(this)}> unMount</button>
+    </div>
+  }
+
+}
+
 ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Wrapper />, document.getElementById('app1'));
